ca190bfaaa141d2971897745289d50596c987072
Sacha <sacha-u@hotmail.com>
2025-03-07 16:19:00 +0000

    working version

--- app/(tabs)/pdfViewer.tsx
+++ app/(tabs)/pdfViewer.tsx
@@ -5,23 +5,22 @@ import { GlobalWorkerOptions } from "pdfjs-dist";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { Text } from "react-native";
 
 const PDFViewer = () => {
-  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-  // useEffect(() => {
-  //   // Set the workerSrc property to point to the worker file
-  //   GlobalWorkerOptions.workerSrc =
-  //     "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
-  // }, []);
+  useEffect(() => {
+    // Set the workerSrc property to point to the worker file
+    GlobalWorkerOptions.workerSrc =
+      "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+  }, []);
 
   return (
     <div style={{ height: "100vh" }}>
-      {/* <Viewer
+      <Viewer
         fileUrl="/Horizontal-content.pdf"
         plugins={[defaultLayoutPluginInstance]}
-      /> */}
+      />
     </div>
   );
 };
