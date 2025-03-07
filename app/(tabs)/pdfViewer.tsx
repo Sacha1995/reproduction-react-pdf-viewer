595ce9655006c0211595aa4786d54a0ceb7bce6b
Sacha <sacha-u@hotmail.com>
2025-03-07 16:25:59 +0000

    comment out error code

--- app/(tabs)/pdfViewer.tsx
+++ app/(tabs)/pdfViewer.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import { GlobalWorkerOptions } from "pdfjs-dist";
 
@@ -7,20 +7,20 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 const PDFViewer = () => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-  useEffect(() => {
-    // Set the workerSrc property to point to the worker file
-    GlobalWorkerOptions.workerSrc =
-      "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
-  }, []);
+  // useEffect(() => {
+  //   // Set the workerSrc property to point to the worker file
+  //   GlobalWorkerOptions.workerSrc =
+  //     "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+  // }, []);
 
   return (
     <div style={{ height: "100vh" }}>
-      <Viewer
+      {/* <Viewer
         fileUrl="/Horizontal-content.pdf"
         plugins={[defaultLayoutPluginInstance]}
-      />
+      /> */}
     </div>
   );
 };
